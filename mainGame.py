2a10ea3ad4fe60be79dfb725d19b9893eddb0621
KristerL <looga.krister@gmail.com>
2018-10-25 14:38:10 +0300

    Lisasin vajalikud funktsioonid, mida on vaja teha

---
+++ mainGame.py
@@ -0,0 +1,33 @@
+pakk = [1,2,3,4,5,6,7,8,9,10,11,12,13,14] # kaardipakk on numbritega, et kergemini arvutada skoori, kuigi 11-14 on tegelt pildikaardid
+
+
+#Main event handlemine toimub siin. Mäng jookseb siin
+def game():
+
+
+#Arvutab skoori ja tagastab selle
+def score():
+
+
+#kui kasutaja valib hit, siis tõmmatakse kaardipakkist kaart (jookseb animatsioon) ja tehakse score arvutus
+def hit():
+
+
+#Kasutaja jätab käigu vahele AI võib teha käigu
+def stand()
+
+#
+# #Kasutaja saab
+# def doubleDown():
+#
+#
+# #Kui aega jääb, siis panused panna mängu
+# def split():
+
+
+#Kasutaja saab ainult esimesel käigul alla anda (automaatselt alla anda)
+def surrender():
+
+
+
+def ai():
