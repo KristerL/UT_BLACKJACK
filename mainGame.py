f33cbcaf57a456411a3c1c2fb7d5706bf97d74c4
KristerL <looga.krister@gmail.com>
2018-10-25 16:18:10 +0300

    testing branches

--- mainGame.py
+++ mainGame.py
@@ -1,6 +1,8 @@
 from random import randint
 
 #main?
+#test
+print(test)
 
 pakk = [1,2,3,4,5,6,7,8,9,10,11,12,13,14]*4 # kaardipakk on numbritega, et kergemini arvutada skoori, kuigi 11-14 on tegelt pildikaardid
 
