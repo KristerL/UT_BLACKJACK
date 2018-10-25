751af84b2eec3a6831de0ed0caee6689d9cd91fb
KristerL <looga.krister@gmail.com>
2018-10-25 16:16:25 +0300

    test

--- mainGame.py
+++ mainGame.py
@@ -1,5 +1,7 @@
 from random import randint
 
+#main?
+
 pakk = [1,2,3,4,5,6,7,8,9,10,11,12,13,14]*4 # kaardipakk on numbritega, et kergemini arvutada skoori, kuigi 11-14 on tegelt pildikaardid
 
 #Trackib mängu jooksul ai ja mängija kaarte
