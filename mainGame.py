b2f97062c2d01ef9b31f7b7246f095b3a96656ad
KristerL <looga.krister@gmail.com>
2018-10-25 15:35:19 +0300

    Täiustasin game ja score funktsioone

--- mainGame.py
+++ mainGame.py
@@ -1,33 +1,85 @@
-pakk = [1,2,3,4,5,6,7,8,9,10,11,12,13,14] # kaardipakk on numbritega, et kergemini arvutada skoori, kuigi 11-14 on tegelt pildikaardid
+from random import randint
+
+pakk = [1,2,3,4,5,6,7,8,9,10,11,12,13,14]*4 # kaardipakk on numbritega, et kergemini arvutada skoori, kuigi 11-14 on tegelt pildikaardid
+
+#Trackib mängu jooksul ai ja mängija kaarte
+player = []
+computer = []
+
+#Trackib mängu jooksul ai ja mängija punkte
+playerPunktid = 0
+computerPunktid = 0
+
 
 
 #Main event handlemine toimub siin. Mäng jookseb siin
 def game():
 
+    player = start()
+    computer = start()
+    playerPunktid = score(player)
+    computerPunktid = score(computer)
+    print(player)
+    print(computer)
+    print(playerPunktid)
+    print(computerPunktid)
+
+
+#Funktsioon annab kätte esimesed kaks kaarti
+def start():
+    list = []
+    kaart = pakk[randint(1,len(pakk))]
+    list.append(kaart)
+    kaart = pakk[randint(1,len(pakk))]
+    list.append(kaart)
+
+    return list
+
+
+
 
 #Arvutab skoori ja tagastab selle
-def score():
+def score(kaardid):
+    punktid = 0
+    for j in range(len(kaardid)):
+            punktid += kaardid[j]
+
+    return punktid
 
 
-#kui kasutaja valib hit, siis tõmmatakse kaardipakkist kaart (jookseb animatsioon) ja tehakse score arvutus
-def hit():
-
-
-#Kasutaja jätab käigu vahele AI võib teha käigu
-def stand()
 
 #
-# #Kasutaja saab
-# def doubleDown():
+#
+# #Kasutaja saab ainult esimesel käigul alla anda (automaatselt alla anda)
+# def surrender():
 #
 #
-# #Kui aega jääb, siis panused panna mängu
-# def split():
+#
+# def ai():
+#
+#
 
+# #kui kasutaja valib hit, siis tõmmatakse kaardipakkist kaart (jookseb animatsioon) ja tehakse score arvutus
+# def hit():
+#
+#
+# #Kasutaja jätab käigu vahele AI võib teha käigu
+# def stand()
+#
+# #
+# # #Kasutaja saab
+# # def doubleDown():
+# #
+# #
+# # #Kui aega jääb, siis panused panna mängu
+# # def split():
+#
+#
+# #Kasutaja saab ainult esimesel käigul alla anda (automaatselt alla anda)
+# def surrender():
+#
+#
+#
+# def ai():
 
-#Kasutaja saab ainult esimesel käigul alla anda (automaatselt alla anda)
-def surrender():
-
-
-
-def ai():
+game()
