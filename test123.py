8bd2e8841a89c54f8369812f5fd5f7be2c3e4025
KristerL <looga.krister@gmail.com>
2018-11-26 20:32:31 +0200

    removed old files

--- test123.py
+++
@@ -1,9 +0,0 @@
-
-import os
-
-for root, dirs, files in os.walk("."):  
-    for filename in files:
-        if filename[-3:] == "png":
-            print(filename)
-            tagurpidi_kaart_pilt = print(filename)
-        
